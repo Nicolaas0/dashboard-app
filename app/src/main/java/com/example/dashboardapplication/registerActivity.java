c619b147668f1eff451f3d23417c19b99962817f
Nicolaas0 <nicolerrick@gmail.com>
2020-12-12 00:00:17 +0700

    add registerActivity page

---
+++ app/src/main/java/com/example/dashboardapplication/registerActivity.java
@@ -0,0 +1,15 @@
+package com.example.dashboardapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        getSupportActionBar().hide();
+    }
+}
