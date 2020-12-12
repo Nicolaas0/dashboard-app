6164fc46b70b2767c81f560431e2c2a1a3f4515b
Nicolaas0 <nicolerrick@gmail.com>
2020-12-12 23:50:52 +0700

    add code to make the username from login to appear in the page

--- app/src/main/java/com/example/dashboardapplication/dashboard.java
+++ app/src/main/java/com/example/dashboardapplication/dashboard.java
@@ -2,13 +2,26 @@ package com.example.dashboardapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import android.os.Bundle;
 
 public class dashboard extends AppCompatActivity {
-
+    TextView nama;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        nama = (TextView)findViewById(R.id.tv_name);
+
+        nama.setText(preferences.getLoggedInUser(getBaseContext()));
     }
 }
