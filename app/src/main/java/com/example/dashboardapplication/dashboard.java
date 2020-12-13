db80837005b4e53a461b9f70490c8f33c672b610
Nicolaas0 <nicolerrick@gmail.com>
2020-12-13 20:32:10 +0700

    solving minor problem

--- app/src/main/java/com/example/dashboardapplication/dashboard.java
+++ app/src/main/java/com/example/dashboardapplication/dashboard.java
@@ -19,6 +19,7 @@ public class dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        getSupportActionBar().hide();
 
         nama = (TextView)findViewById(R.id.tv_name);
 
