c93301b67673ed469beeafe8412347f802ee2266
Nicolaas0 <nicolerrick@gmail.com>
2020-12-25 21:23:03 +0700

    model for user

---
+++ app/src/main/java/com/example/dashboardapplication/model/User.java
@@ -0,0 +1,46 @@
+package com.example.dashboardapplication.model;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.io.Serializable;
+@IgnoreExtraProperties
+public class User implements Serializable {
+    private String name;
+    private String password;
+    private String Repassword;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRepassword() {
+        return Repassword;
+    }
+
+    public void setRepassword(String repassword) {
+        Repassword = repassword;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String  toString(){
+        return " " + name + "\n" +
+                " " + password;
+    }
+
+    public User(String s, String nm, String pss){
+        name = nm;
+        password = pss;
+    }
+}
